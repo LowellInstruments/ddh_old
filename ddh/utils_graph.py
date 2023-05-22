38f6e654e1eae76f5bb68f4d0b1ba62179ece75e
kazola <kazola>
2023-05-22 14:34:02 -0400

    moregraph

--- ddh/utils_graph.py
+++ ddh/utils_graph.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import sys
 from PyQt5 import QtWidgets
@@ -6,6 +7,33 @@ import pyqtgraph as pg
 
 
 class SeparateGraphWindow(QtWidgets.QMainWindow):
+
+    def _check_graph_req_file_at_boot(self):
+        rv = 0
+        try:
+            # file written by DDH plot request
+            with open('/tmp/graph_req.json') as f:
+                self.fol = f.read().strip()
+            if not os.path.exists(self.fol):
+                print('graph: error _at_boot, bad_fol {}'.format(self.fol))
+                os._exit(1)
+            return self.fol
+        except (Exception, ) as ex:
+            print('graph: error _at_boot, exception', ex)
+            os._exit(1)
+
+    @staticmethod
+    def _graph_infer_folder_logger_metric(path):
+        ff_t = glob.glob("{}/{}".format(path, "*_Temperature.csv"))
+        ff_p = glob.glob("{}/{}".format(path, "*_Pressure.csv"))
+        ff_do = glob.glob("{}/{}".format(path, "*_DissolvedOxygen.csv"))
+        if ff_do and (ff_t or ff_p):
+            print('graph: error mixed files in ')
+            os._exit(1)
+        if ff_t:
+            return "_Temperature.csv"
+        if ff_do:
+            return "_DissolvedOxygen.csv"
 
     def _btn_close_click(self):
         print('closing graph window')
@@ -18,13 +46,7 @@ class SeparateGraphWindow(QtWidgets.QMainWindow):
     def _btn_next_span_click(self):
         print('next_span')
 
-    def _check(self):
-        if not self.fol_ls:
-            self.g.setTitle("error: empty folder list", color="r", size="15pt")
-        # todo: do folder setting on pressing 1 and on auto-download
-        fol = self.fol_ls[0]
-
-    def _plot_line(self, x, y, plotname, color):
+    def _graph_line(self, x, y, plotname, color):
         pen = pg.mkPen(color=color)
         self.g.plot(x, y,
                     name=plotname,
@@ -35,6 +57,11 @@ class SeparateGraphWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(SeparateGraphWindow, self).__init__(*args, **kwargs)
+
+        # grab folder to plot
+        self.fol_ls = self._get_list_of_all_mac_folders()
+        self.fol = self._check_graph_req_file_at_boot()
+        self.met = self._graph_infer_folder_logger_metric(self.fol)
 
         # controls
         self.btn_close = QPushButton('Quit', self)
@@ -59,15 +86,6 @@ class SeparateGraphWindow(QtWidgets.QMainWindow):
         wid.setLayout(vl)
         self.g.setBackground('w')
 
-        # debug: variables
-        self.span = 'h'
-        self.fol_ls = [f.path for f in os.scandir('../dl_files') if f.is_dir()]
-        self.fol_ls = [f for f in self.fol_ls if "ddh" not in f]
-
-        # check everything all right
-        if not self._check():
-            return
-
         # plot the data
         self.g.setTitle("Your Title Here", color="b", size="15pt")
         styles = {"color": "#f00", "font-size": "20px"}
@@ -80,8 +98,8 @@ class SeparateGraphWindow(QtWidgets.QMainWindow):
         temperature_2 = [50,35,44,22,38,32,27,38,32,44]
         self.g.setXRange(0, 10, padding=0)
         self.g.setYRange(20, 55, padding=0)
-        self._plot_line(hour, temperature_1, "Sensor1", 'r')
-        self._plot_line(hour, temperature_2, "Sensor2", 'b')
+        self._graph_line(hour, temperature_1, "Sensor1", 'r')
+        self._graph_line(hour, temperature_2, "Sensor2", 'b')
 
 
 # to test
