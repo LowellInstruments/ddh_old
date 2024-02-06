4d01e7cfec4d634775ae8fc4ed65969c3a2fbc61
kaz <kaz>
2024-02-06 12:28:04 -0500

    more

--- main_api.py
+++ main_api.py
@@ -178,6 +178,8 @@ async def ep_update_ddh():
 @app.get('/update_mat')
 async def ep_update_mat():
     d = api_get_folder_path_root()
+    print('d', d)
+    print('full', 'cd {d}/scripts && ./pop_mat.sh')
     return _ep_update('update_mat', f'cd {d}/scripts && ./pop_mat.sh')
 
 
