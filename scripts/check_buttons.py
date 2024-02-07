87b4b5b265414d2cdda3c658c535c52eba39a8fc
kaz <kaz>
2024-02-07 13:12:55 -0500

    more

--- scripts/check_buttons.py
+++ scripts/check_buttons.py
@@ -63,6 +63,4 @@ button_c.when_released = button_c_released_cb
 print(f'bounce times A {0} B {btb} C {btc}')
 print(f'pullup confs A {pua} B {pub} C {puc}')
 while True:
-    # callbacks will be called
-    print(".")
-    time.sleep(1)
+    pass
