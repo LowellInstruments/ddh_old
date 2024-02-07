65cd29f77622b1c19bdd0849fe0ac9022a0b5a82
kaz <kaz>
2024-02-07 12:43:40 -0500

    more

--- scripts/pop_ddh.sh
+++ scripts/pop_ddh.sh
@@ -59,6 +59,7 @@ echo "$FOL_DDT" >> /tmp/dbg.txt && \
 cp "$FOL_DDT"/_dt_files/ble_dl_moana.py "$FOL_DDH"/dds && \
 echo "d" >> /tmp/dbg.txt
 _e $? "$_S"
+echo "w" >> /tmp/dbg.txt
 
 
 _pg "[ POP ] ddh | ran OK!"
