f0a8f2c6817e37412888bf72fa43332aa49758ec
kaz <kaz>
2024-02-12 14:36:39 -0500

    more

--- dds/aws.py
+++ dds/aws.py
@@ -99,9 +99,9 @@ def _aws_s3_sync_process():
             '--include "*.lid" '
             '--include "*.lix" '
             '--include "*.bin" '
-            '--exclude "test_*.csv '
-            '--exclude "test_*.lid '
-            '--exclude "test_*.lix '
+            '--exclude "test_*.csv" '
+            '--exclude "test_*.lid" '
+            '--exclude "test_*.lix" '
             '--include "*.txt" {}'
         ).format(_k, _s, _bin, m, _n, um, dr)
 
@@ -123,9 +123,9 @@ def _aws_s3_sync_process():
                 '--include "*.lid" '
                 '--include "*.lix" '
                 '--include "*.bin" '
-                '--exclude "test_*.csv '
-                '--exclude "test_*.lid '
-                '--exclude "test_*.lix '
+                '--exclude "test_*.csv" '
+                '--exclude "test_*.lid" '
+                '--exclude "test_*.lix" '
                 '--include "*.txt" {}'
             ).format(_k, _s, _bin, m, _n, um, dr)
 
