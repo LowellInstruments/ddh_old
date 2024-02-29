7c623713d3c00347de4521a8f1a3ffbe82898143
kaz <kaz>
2024-02-29 05:13:34 -0500

    more

--- scripts/run_script_ddh_kill.sh
+++ scripts/run_script_ddh_kill.sh
@@ -8,5 +8,6 @@ pkill -F /tmp/main_dds_controller.pid 2> /dev/null
 pkill -F /tmp/main_ddh_controller.pid 2> /dev/null
 pkill -F /tmp/main_dds.pid 2> /dev/null
 pkill -F /tmp/main_ddh.pid 2> /dev/null
+killall main_api
 
 echo 'done DDH kill'
