e48bc800cd8a279431c9da15a64182cd58b0a88c
kaz <kaz@kazs-Laptop.local>
2024-03-24 09:35:44 -0400

    more

---
+++ main_check.py
@@ -0,0 +1,23 @@
+# check rpi
+#   - release year os
+#   - arch
+#   - 3 or 4
+#   - hostname
+#
+# origin clone
+#   - rpi-clone
+#   - balena (JQ flag)
+#
+# ddh
+#   - pure (lsusb SDD)
+#   - emolt
+#
+#
+# compare flags
+#   - emolt vs configuration flag rbl_en
+#
+#
+# compare conf toml vs lsusb (gps, shield, rockblocks)
+#
+# bluez version
+# hciuart modified md5sum
