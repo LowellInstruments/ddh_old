0525069e7417e2718dfa828d0628d461c80174e2
kaz <kaz>
2024-04-10 10:09:49 -0400

    more

--- dds/state.py
+++ dds/state.py
@@ -3,6 +3,11 @@ from mat.ble.ble_mat_utils import ble_mat_get_antenna_type_v2
 
 class DdhState:
     def __init__(self):
+        # some things are not ready at boot
+        self.ble_antenna_i = None
+        self.ble_antenna_s = None
+
+    def refresh(self):
         self.ble_antenna_i, self.ble_antenna_s = ble_mat_get_antenna_type_v2()
 
 
