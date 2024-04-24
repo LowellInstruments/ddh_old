c3aa0ebe0b5b0216aaba2600b5ccb26ae4272861
kaz <kaz>
2024-04-24 12:24:01 -0400

    more

--- main_ddp.py
+++ main_ddp.py
@@ -15,6 +15,8 @@ def _e(e):
 
 def sh(c):
     rv = sp.run(c, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    if rv.returncode:
+        print(rv.stderr)
     return rv.returncode
 
 
@@ -27,7 +29,8 @@ h = str(pathlib.Path.home())
 p = 'li/ddh' if is_rpi() else 'PycharmProjects/ddh'
 path_script_deploy_dox = f'{h}/{p}/scripts/run_script_deploy_logger_dox.sh'
 path_script_deploy_tdo = f'{h}/{p}/scripts/run_script_deploy_logger_tdo.sh'
-print(path_script_deploy_dox)
+path_script_brt = f'{h}/{p}/scripts/main_brt.pyc'
+path_script_nadv = f'{h}/{p}/scripts/main_nadv.pyc'
 
 
 def cb_test_gps_quectel():
@@ -53,6 +56,16 @@ def cb_test_buttons():
         main_test_box_buttons()
     except (Exception, ) as ex:
         _e(str(ex))
+
+
+def cb_run_brt():
+    c = f'/usr/bin/env python {path_script_brt}'
+    sh(c)
+
+
+def cb_run_nadv():
+    c = f'/usr/bin/env python {path_script_nadv}'
+    sh(c)
 
 
 def cb_run_deploy_dox():
@@ -82,9 +95,11 @@ if __name__ == "__main__":
         d = {
             "0) test GPS shield": (0, cb_test_gps_quectel),
             "1) test side buttons": (1, cb_test_buttons),
-            "2) deploy logger DOX": (2, cb_run_deploy_dox),
-            "3) deploy logger TDO": (3, cb_run_deploy_tdo),
-            "4) quit": (4, cb_quit)
+            "2) run BLE range tool": (2, cb_run_brt),
+            "3) run BLE n_adv tool": (3, cb_run_nadv),
+            "4) deploy logger DOX": (4, cb_run_deploy_dox),
+            "5) deploy logger TDO": (5, cb_run_deploy_tdo),
+            "6) quit": (6, cb_quit)
         }
         for i in d.keys():
             print(f'\t{i}')
