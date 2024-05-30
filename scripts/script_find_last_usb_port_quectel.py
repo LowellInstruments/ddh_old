8fdd9762ce875567393eab0455e54a730fd11b34
kaz <kaz>
2024-05-30 13:56:37 -0400

    more

--- scripts/script_find_last_usb_port_quectel.py
+++ scripts/script_find_last_usb_port_quectel.py
@@ -12,9 +12,5 @@ for p in serial.tools.list_ports.comports():
 
 if ls:
     v = ls[-2]
-    c = f'echo -ne "AT+QCCID\r" > /dev/{v}'
-    print(c)
-    sp.run(c, shell=True, stderr=sp.PIPE, stdout=sp.PIPE)
-    time.sleep(.1)
-    c = f'timeout 1 cat -v < /dev/{v} | grep QCCID > /home/pi/li/.iccid'
+    c = f'echo -ne "AT+QCCID\\r" > /dev/{v} && (cat -v < /dev/{v} | grep QCCID > /home/pi/li/.iccid)'
     rv = sp.run(c, shell=True, stderr=sp.PIPE, stdout=sp.PIPE)
