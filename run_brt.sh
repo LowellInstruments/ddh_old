6923469e3f99e04b21bf8b10b041c9db82cd6056
kaz <kaz>
2024-07-01 10:22:16 -0400

    toml

--- run_brt.sh
+++ run_brt.sh
@@ -12,5 +12,8 @@ _pb "###############"
 _pb "     DDH BRT   "
 _pb "###############"
 echo
+
+cp "$FOL_LI"/brt/main_brt.py "$FOL_DDH"
+
 source "$FOL_VEN"/bin/activate
 cd "$FOL_DDH" && "$FOL_VEN"/bin/python main_brt.py
