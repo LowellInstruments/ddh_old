3420e5a0ae8b971c8984afc5f13d556fa48b237d
kaz <kaz>
2024-08-16 09:48:14 -0400

    toml

--- main_api.py
+++ main_api.py
@@ -18,7 +18,7 @@ from api.api_utils import (api_get_ip_vpn, api_get_ip_wlan, api_get_ip_cell,
                            CTT_API_ER, api_get_uptime_secs, api_ddh_get_folder_dl_files,
                            api_get_ddh_folder_path_macs_black, api_get_ddh_sw_version,
                            api_get_utc_epoch, api_get_api_version, api_get_ble_iface,
-                           get_files_from_server, api_get_gps_iface,
+                           get_files_from_server, api_get_gps_iface, api_get_fw_cell_version,
                            )
 from ddh.db.db_his import DbHis
 from utils.ddh_config import (dds_get_cfg_vessel_name,
@@ -164,6 +164,7 @@ async def api_get_info():
         "time_zone": _th(api_get_timezone),
         "ddh_version": _th(api_get_ddh_sw_version),
         "api_version": _th(api_get_api_version),
+        "cell_fw_ver": _th(api_get_fw_cell_version)
         # "commit_mat": _th(get_git_commit_mat_local),
         # "commit_ddh": _th(get_git_commit_ddh_local),
     }
