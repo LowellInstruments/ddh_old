30eb4b888b52a08131c71cbaaa7010ca2688dbb4
kaz <kaz>
2024-08-28 14:11:33 -0400

    toml

--- scripts/script_logger_tdo_deploy_utils.py
+++ scripts/script_logger_tdo_deploy_utils.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+
+import toml
 from bleak import BleakScanner, BleakError
 from bleak.backends.device import BLEDevice
 from mat.ble.bleak.cc26x2r import BleCC26X2
@@ -15,7 +17,7 @@ def _e(_rv, s):
         raise Exception(_.format(s, _rv))
 
 
-async def deploy_logger_tdo(mac, sn, cfg):
+async def deploy_logger_tdo(mac, sn, cfg_from_menu):
 
     rv = 0
 
@@ -26,6 +28,8 @@ async def deploy_logger_tdo(mac, sn, cfg):
         # firmware version the first in case fails
         rv, v = await lc.cmd_gfv()
         _e(rv, "gfv")
+        ver = v
+        print('ver', ver)
 
         g = ("-3.333333", "-4.444444", None, None)
         rv = await lc.cmd_sws(g)
@@ -50,10 +54,6 @@ async def deploy_logger_tdo(mac, sn, cfg):
         _e(rv, "wli_ba")
         await asyncio.sleep(.1)
 
-        rv = await lc.cmd_wli("MA1234ABC")
-        _e(rv, "wli_ma")
-        await asyncio.sleep(.1)
-
         rv = await lc.cmd_wli("CA1234")
         _e(rv, "wli_ca")
         await asyncio.sleep(.1)
@@ -64,14 +64,9 @@ async def deploy_logger_tdo(mac, sn, cfg):
         await asyncio.sleep(.1)
 
         rv, info = await lc.cmd_rli()
-        _e(len(info.keys()) != 3, "rli")
+        # _e(len(info.keys()) != 3, "rli")
 
-        #rv = await lc.cmd_wak("on")
-        #if rv:
-        #    rv = await lc.cmd_wak("on")
-        #_e(rv, "wak")
-
-        # these stand for First Deployment Get / Set on TDO loggers
+        # First Deployment Get / Set on TDO loggers
         rv, v = await lc.cmd_fdg()
         _e(rv, 'fdg')
         rv = await lc.cmd_fds()
@@ -84,33 +79,42 @@ async def deploy_logger_tdo(mac, sn, cfg):
         _e(bad_rv, "bat")
         print("\t\tBAT | {} mV".format(b))
 
-        # -------------------------------------
-        # profiling mode set
-        # -------------------------------------
-        prf_json_file = cfg['PRF']
-        with open(prf_json_file) as f:
-            # file to dict
-            j_i = json.load(f)
-            print(f'SCF: loaded {prf_json_file}')
+        # -----------------------------
+        # new loggers with unified SCC
+        # -----------------------------
+        prf_file = cfg_from_menu['PRF']
+        if ver >= "4.0.06":
+            # send profiling configuration, new files are toml
+            prf_file = prf_file.replace('.json', '.toml')
+            print('prf_file', prf_file)
+            with open(prf_file, 'r') as f:
+                d = toml.load(f)['profiling']
+        else:
+            # not present in newer loggers
+            rv = await lc.cmd_wli("MA1234ABC")
+            _e(rv, "wli_ma")
+            await asyncio.sleep(.1)
+            # send profiling configuration
+            with open(prf_file) as f:
+                d = json.load(f)
 
-            # flash SCF into logger
-            for tag, v in j_i.items():
-                if len(tag) != 3:
-                    print(f'error: bad SCF tag {tag}')
-                    break
-                if len(v) != 5:
-                    print(f'error: bad SCF value {v} for tag {tag}')
-                    break
-
-                # send the SCF command
-                rv = await lc.cmd_scf(tag, v)
-                bad_rv = rv == 1
-                _e(bad_rv, f"scf {tag}")
+        # send the SCF commands
+        print(f'SCF: loaded {prf_file}')
+        for tag, v in d.items():
+            if len(tag) != 3:
+                print(f'error: bad SCF tag {tag}')
+                break
+            if len(v) != 5:
+                print(f'error: bad SCF value {v} for tag {tag}')
+                break
+            rv = await lc.cmd_scf(tag, v)
+            bad_rv = rv == 1
+            _e(bad_rv, f"scf {tag}")
 
         # -------------------------------
         # RUNs logger, depending on flag
         # -------------------------------
-        if cfg['RUN']:
+        if cfg_from_menu['RUN']:
             await asyncio.sleep(1)
             g = (1.111111, 2.222222, None, None)
             rv = await lc.cmd_rws(g)
