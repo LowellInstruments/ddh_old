7d9ab33928da3994e9536650e05488af5e5c8e13
kaz <kaz>
2024-09-05 12:14:00 -0400

    toml

--- dds/gps.py
+++ dds/gps.py
@@ -269,7 +269,7 @@ def _gps_measure():
             g = _gps_parse_rmc_frame(b)
 
         # todo ---> let's define these conditions
-        if (g and ns and
+        # if (g and ns and
             #     is_it_time_to('send_notif_gps_num_satellites', PERIOD_GPS_NOTI_NUM_GPS_SAT)):
             # notify_ddh_number_of_gps_satellites(g, ns)
 
