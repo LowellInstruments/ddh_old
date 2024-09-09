38d4ca0ace330e4696d64081e14f32328b4a1223
kaz <kaz>
2024-09-09 12:52:03 -0400

    toml

--- dds/gps.py
+++ dds/gps.py
@@ -6,7 +6,7 @@ import time
 import serial
 
 from dds.gpq import GpqW
-from dds.notifications import notify_ddh_error_hw_gps
+from dds.notifications import notify_ddh_error_hw_gps, notify_ddh_number_of_gps_satellites
 from dds.timecache import is_it_time_to
 from mat.gps import PORT_CTRL, PORT_DATA
 from mat.utils import linux_is_rpi, linux_set_datetime
@@ -48,6 +48,7 @@ PERIOD_GPS_AT_BOOT_SECS = 600
 PERIOD_GPS_TELL_GPS_HW_ERROR_SECS = 3600 * 3
 PERIOD_GPS_TELL_PUCK_NO_PC = 3600 * 6
 PERIOD_GPS_POWER_CYCLE = 300
+PERIOD_GPS_NOTI_NUM_GPS_SAT = 1800
 
 
 # some emolt boxes have too many USB ports
@@ -167,7 +168,7 @@ def _gps_parse_gsv_frame(data: bytes, force_print=False):
 
     # data: b'$GPGSV,...,$GPGGA,...' all mixed
     if b"GPGSV" not in data:
-        return
+        return -1
 
     # $GPGSV, #messages, msg_num, num sat, ...
     data = data.decode()
@@ -182,9 +183,11 @@ def _gps_parse_gsv_frame(data: bytes, force_print=False):
             _u("{}/{}".format(STATE_DDS_NOTIFY_GPS_NUM_SAT, n))
             if n < 7:
                 lg.a("{} satellites in view".format(n))
+        return n
 
     except (Exception,) as ex:
         lg.a("error: parse GSV frame {} -> {}".format(data, ex))
+        return -1
 
 
 def _gps_measure():
@@ -231,6 +234,8 @@ def _gps_measure():
     # loop waiting for GPS frames
     # =============================
     till = time.perf_counter() + 2
+    b = bytes()
+    ns = -1
     while 1:
         if time.perf_counter() > till:
             break
@@ -257,11 +262,11 @@ def _gps_measure():
                 g = _gps_parse_rmc_frame(b"$GPRMC" + re_rmc.group(1))
             re_gsv = re.search(b"GPGSV(.*)\r\n", b)
             if re_gsv:
-                _gps_parse_gsv_frame(b"$GPGSV" + re_gsv.group(1))
+                ns = _gps_parse_gsv_frame(b"$GPGSV" + re_gsv.group(1))
 
         # GPS shield
         else:
-            _gps_parse_gsv_frame(b)
+            ns = _gps_parse_gsv_frame(b)
             g = _gps_parse_rmc_frame(b)
 
         if g:
@@ -282,6 +287,11 @@ def _gps_measure():
         _u(f"{STATE_DDS_NOTIFY_GPS}/{lat}, {lon}")
         _g_ts_cached_gps_valid_for = now + PERIOD_GPS_CACHE_VALID_SECS
         _g_cached_gps = lat, lon, g[2], float(g[3])
+
+        # do the number of satellites notification
+        # if is_it_time_to('SQS_gps_num_satellites', PERIOD_GPS_NOTI_NUM_GPS_SAT):
+        #     notify_ddh_number_of_gps_satellites(ns)
+
         return g
 
     # failed, and it's the first time ever
