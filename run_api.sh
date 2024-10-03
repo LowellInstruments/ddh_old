58df0c3a92efd5b7695a9a66cbb3dde2702be55a
kaz <kaz>
2024-10-03 09:23:13 -0400

    toml

--- run_api.sh
+++ run_api.sh
@@ -6,7 +6,7 @@ clear
 
 # for crontab to detect already running
 check_already_running "main_api_controller"
-# check_already_running "requirements_api"
+check_already_running "requirements_api"
 
 
 echo && echo
@@ -16,5 +16,6 @@ _pb "###############"
 echo
 sudo chown -R pi:pi "$FOL_LI"
 source "$FOL_VAN"/bin/activate
+pip install --no-cache-dir "$FOL_DDT"/_dt_files/my_wheels/botocore-1.29.165-py3-none-any.whl
 pip install -r "$FOL_DDH"/requirements_api.txt
 cd "$FOL_DDH" && "$FOL_VAN"/bin/python main_api.py
