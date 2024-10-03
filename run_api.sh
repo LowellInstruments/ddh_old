649846fe67089c181c81236c3bc9505dab89d810
kaz <kaz>
2024-10-03 09:35:08 -0400

    toml

--- run_api.sh
+++ run_api.sh
@@ -16,6 +16,8 @@ _pb "###############"
 echo
 sudo chown -R pi:pi "$FOL_LI"
 source "$FOL_VAN"/bin/activate
-pip install --no-cache-dir "$FOL_DDT"/_dt_files/my_wheels/botocore-1.29.165-py3-none-any.whl
+# slightly different version than the DDH one
+pip install --no-cache-dir "$FOL_DDT"/_dt_files/my_wheels/boto3-1.35.32-py3-none-any.whl
+pip install --no-cache-dir "$FOL_DDT"/_dt_files/my_wheels/botocore-1.35.32-py3-none-any.whl
 pip install -r "$FOL_DDH"/requirements_api.txt
 cd "$FOL_DDH" && "$FOL_VAN"/bin/python main_api.py
